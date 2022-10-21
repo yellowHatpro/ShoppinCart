69e7a7adce0e38719713ff532815e7de81cfdeb3
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-10-21 20:04:56 +0530

    added app directory and android project init

---
+++ ShoppinCartAndroid/app/src/main/java/org/yellowhatpro/shoppincart/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.yellowhatpro.shoppincart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import org.yellowhatpro.shoppincart.ui.theme.ShoppinCartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppinCartTheme {
+            }
+        }
+    }
+}
