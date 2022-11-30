146644a18888f248b39230cb9e2299bb176354d4
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-11-30 11:36:48 +0530

    sign in working

--- ShoppinCartAndroid/app/src/main/java/org/yellowhatpro/shoppincart/presentation/MainActivity.kt
+++ ShoppinCartAndroid/app/src/main/java/org/yellowhatpro/shoppincart/presentation/MainActivity.kt
@@ -1,43 +1,78 @@
 package org.yellowhatpro.shoppincart.presentation
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.AndroidEntryPoint
+import org.yellowhatpro.shoppincart.R
 import org.yellowhatpro.shoppincart.presentation.ui.components.BottomBar
 import org.yellowhatpro.shoppincart.presentation.theme.ShoppinCartTheme
 import org.yellowhatpro.shoppincart.presentation.ui.components.TopAppBar
+import org.yellowhatpro.shoppincart.presentation.ui.features.auth.AuthScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = this.getSharedPreferences("uid", Context.MODE_PRIVATE)
         setContent {
+            var authStatus by rememberSaveable {
+                mutableStateOf(false)
+            }
+            if (!getSharedPreferences("uid", MODE_PRIVATE).getString("uid", "").isNullOrEmpty()) {
+                authStatus = true
+            }
+
             val navController = rememberNavController()
             val viewModel = hiltViewModel<ShoppinCartViewModel>()
-            ShoppinCartTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(navController)
-                    },
-                    bottomBar = {
-                        BottomBar(navController = navController)
+            if (!authStatus) {
+                AuthScreen(getGoogleLoginAuth(), sharedPreferences, this)
+            } else {
+                ShoppinCartTheme {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(navController)
+                        },
+                        bottomBar = {
+                            BottomBar(navController = navController)
+                        }
+                    ) { paddingValues ->
+
+                        ShoppinCartNavigation(
+                            navHostController = navController,
+                            viewModel = viewModel,
+                            modifier = Modifier.padding(paddingValues)
+                        )
                     }
-                ) { paddingValues ->
-                    ShoppinCartNavigation(
-                        navHostController = navController,
-                        viewModel = viewModel,
-                        modifier = Modifier.padding(paddingValues)
-                    )
                 }
             }
         }
+    }
+
+    private fun getGoogleLoginAuth(): GoogleSignInClient {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.google_cloud_server_client_id))
+            .requestId()
+            .requestProfile()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
     }
 }
---
+++ ShoppinCartAndroid/app/src/main/java/org/yellowhatpro/shoppincart/presentation/ui/features/auth/AuthScreen.kt
@@ -0,0 +1,93 @@
+package org.yellowhatpro.shoppincart.presentation.ui.features.auth
+
+import android.app.Activity
+import android.content.SharedPreferences
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import org.yellowhatpro.shoppincart.presentation.MainActivity
+
+
+@Composable
+fun AuthScreen(getGoogleSignInClient : GoogleSignInClient?, sharedPreferences: SharedPreferences, activity: Activity) {
+    Log.d("whatisthis",getGoogleSignInClient?.signInIntent.toString())
+    val startForResult =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult: ActivityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                val intent = activityResult.data
+                if (activityResult.data != null) {
+                    val task: Task<GoogleSignInAccount> =
+                        GoogleSignIn.getSignedInAccountFromIntent(intent)
+                    handleSignInResult(task, sharedPreferences, activity)
+                    Log.d("Login","okay")
+                } else {
+                    Log.d("Login","error")
+                }
+            } else {
+                Log.d("activityResult",activityResult.resultCode.toString())
+            }
+        }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(
+            onClick = {
+                startForResult.launch(getGoogleSignInClient?.signInIntent)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp)
+                .align(Alignment.Center),
+            shape = RoundedCornerShape(6.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
+        }
+    }
+
+}
+
+fun handleSignInResult(completedTask: Task<GoogleSignInAccount>, sharedPreferences: SharedPreferences, activity: Activity) {
+    try {
+        val account = completedTask.getResult(ApiException::class.java)
+
+        // Signed in successfully, show authenticated UI.
+        updateUI(account, sharedPreferences, activity)
+    } catch (e: ApiException) {
+        // The ApiException status code indicates the detailed failure reason.
+        // Please refer to the GoogleSignInStatusCodes class reference for more information.
+        Log.w("SignIn", "signInResult:failed code=" + e.statusCode)
+        updateUI(null, sharedPreferences, activity)
+    }
+}
+
+private fun updateUI(googleSignInAccount: GoogleSignInAccount?, sharedPreferences: SharedPreferences,activity: Activity){
+    if (googleSignInAccount!=null){
+        sharedPreferences.edit().putString("uid", googleSignInAccount.id).apply()
+        Toast.makeText(activity, "Sign in Successful", Toast.LENGTH_SHORT).show()
+        (activity as MainActivity).recreate()
+    }
+}
+
+
+
